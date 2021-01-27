1026184fdba58a701289a80ddadb59a9f24003b0
venkateshnichenametla <77480058+venkateshnichenametla@users.noreply.github.com>
2021-01-27 18:06:52 +0530

    Update Statistics.cs
    Implemented the class to make sure all the testcases are passing

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -1,12 +1,37 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Statistics
 {
     public class StatsComputer
     {
-        public Stats CalculateStatistics(List<float> numbers) {
+        public Stats CalculateStatistics(List<float> numbers) 
+        {
             //Implement statistics here
+            Stats stats = new Stats();
+            if(numbers.Count > 0)
+            {
+                stats.average = numbers.Average();
+                stats.min = numbers.Min();
+                stats.max = numbers.Max();
+            }
+            else
+            {
+                stats.average = float.NaN;
+                stats.max = float.NaN;
+                stats.min = float.NaN;
+            }
+            return stats;
         }
+    }
+
+    public class Stats
+    {
+        public double average { get; set; }
+
+        public double min { get; set; }
+
+        public double max { get; set; }
     }
 }
